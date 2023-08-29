4c7cf976880f8f3602c55548b5c7e96abcfce936
Wajid <ansariwajid9999@gmail.com>
2023-08-29 20:46:25 +0530

    first commit

--- src/main/java/com/driver/services/impl/AdminServiceImpl.java
+++ src/main/java/com/driver/services/impl/AdminServiceImpl.java
@@ -1,8 +1,13 @@
 package com.driver.services.impl;
 
+import com.driver.model.Admin;
+import com.driver.model.Country;
+import com.driver.model.CountryName;
+import com.driver.model.ServiceProvider;
 import com.driver.repository.AdminRepository;
 import com.driver.repository.CountryRepository;
 import com.driver.repository.ServiceProviderRepository;
+import com.driver.repository.UserRepository;
 import com.driver.services.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,16 +22,71 @@ public class AdminServiceImpl implements AdminService {
 
     @Autowired
     CountryRepository countryRepository1;
+    @Autowired
+    private UserRepository userRepository;
 
     @Override
     public Admin register(String username, String password) {
+        Admin admin = new Admin();
+        admin.setUsername(username);
+        admin.setPassword(password);
+
+        adminRepository1.save(admin);
+        return admin;
     }
 
     @Override
     public Admin addServiceProvider(int adminId, String providerName) {
+        Admin admin = adminRepository1.findById(adminId).get();
+        ServiceProvider serviceProvider = new ServiceProvider();
+
+        serviceProvider.setAdmin(admin);
+        serviceProvider.setName(providerName);
+        admin.getServiceProviders().add(serviceProvider);
+        adminRepository1.save(admin);
+        return admin;
     }
 
     @Override
     public ServiceProvider addCountry(int serviceProviderId, String countryName) throws Exception{
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<countryName.length();i++){
+            char c=countryName.charAt(i);
+            if(c>='a'&&c<='z'){
+                c=(char)(c-32);
+            }
+            sb.append(c);
+        }
+        String cname= sb.toString();
+        if(cname.equals("IND")||cname.equals("USA")||cname.equals("JPN")||cname.equals("CHI")||cname.equals("AUS")){
+            Country country=new Country();
+            ServiceProvider serviceProvider=serviceProviderRepository1.findById(serviceProviderId).get();
+            if(cname.equals("IND")){
+                country.setCountryName(CountryName.IND);
+                country.setCode(CountryName.IND.toCode());
+            }
+            if(cname.equals("USA")){
+                country.setCountryName(CountryName.USA);
+                country.setCode(CountryName.USA.toCode());
+            }
+            if(cname.equals("JPN")){
+                country.setCountryName(CountryName.JPN);
+                country.setCode(CountryName.JPN.toCode());
+            }
+            if(cname.equals("CHI")){
+                country.setCountryName(CountryName.CHI);
+                country.setCode(CountryName.CHI.toCode());
+            }
+            if(cname.equals("AUS")){
+                country.setCountryName(CountryName.AUS);
+                country.setCode(CountryName.AUS.toCode());
+            }
+            country.setServiceProvider(serviceProvider);
+            serviceProvider.getCountryList().add(country);
+            serviceProviderRepository1.save(serviceProvider);
+            return serviceProvider;
+        }else{
+            throw new Exception("Country not found");
+        }
     }
 }
--- src/main/java/com/driver/services/impl/UserServiceImpl.java
+++ src/main/java/com/driver/services/impl/UserServiceImpl.java
@@ -1,5 +1,9 @@
 package com.driver.services.impl;
 
+import com.driver.model.Country;
+import com.driver.model.CountryName;
+import com.driver.model.ServiceProvider;
+import com.driver.model.User;
 import com.driver.repository.CountryRepository;
 import com.driver.repository.ServiceProviderRepository;
 import com.driver.repository.UserRepository;
@@ -19,11 +23,68 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User register(String username, String password, String countryName) throws Exception{
+        //create a user of given country. The originalIp of the user should be "countryCode.userId" and return the user.
+        // Note that right now user is not connected and thus connected would be false and maskedIp would be null
+        //Note that the userId is created automatically by the repository layer
+        User user=new User();
 
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<countryName.length();i++){
+            char c=countryName.charAt(i);
+            if(c>='a'&&c<='z'){
+                c=(char)(c-32);
+            }
+            sb.append(c);
+        }
+        String cname= sb.toString();
+        if(cname.equals("IND")||cname.equals("USA")||cname.equals("JPN")||cname.equals("CHI")||cname.equals("AUS")){
+            user.setUsername(username);
+            user.setPassword(password);
+            Country country=new Country();
+
+            if(cname.equals("IND")){
+                country.setCountryName(CountryName.IND);
+                country.setCode(CountryName.IND.toCode());
+            }
+            if(cname.equals("USA")){
+                country.setCountryName(CountryName.USA);
+                country.setCode(CountryName.USA.toCode());
+            }
+            if(cname.equals("JPN")){
+                country.setCountryName(CountryName.JPN);
+                country.setCode(CountryName.JPN.toCode());
+            }
+            if(cname.equals("CHI")){
+                country.setCountryName(CountryName.CHI);
+                country.setCode(CountryName.CHI.toCode());
+            }
+            if(cname.equals("AUS")){
+                country.setCountryName(CountryName.AUS);
+                country.setCode(CountryName.AUS.toCode());
+            }
+            country.setUser(user);
+            user.setOriginalCountry(country);
+            user.setConnected(false);
+
+            String code=country.getCode()+"."+userRepository3.save(user).getId();
+            user.setOriginalIp(code);
+            userRepository3.save(user);
+
+        }else{
+            throw new Exception("Country not found");
+        }
+        return user;
     }
 
     @Override
     public User subscribe(Integer userId, Integer serviceProviderId) {
-
+        //subscribe to the serviceProvider by adding it to the list of providers and return updated User
+        ServiceProvider serviceProvider=serviceProviderRepository3.findById(serviceProviderId).get();
+        User user=userRepository3.findById(userId).get();
+        user.getServiceProviderList().add(serviceProvider);
+        serviceProvider.getUsers().add(user);
+        serviceProviderRepository3.save(serviceProvider);
+        userRepository3.save(user);
+        return user;
     }
 }
